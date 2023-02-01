b6ac1b9d85693bf72a09857ea5e121b09c4ca372
akanksha <akanksha_be21it005@srmcem.ac.in>
2023-02-01 20:12:08 +0530

    io

---
+++ info.py
@@ -0,0 +1,9 @@
+name=input("Enter name ")
+dob=input("Enter Date of Birth")
+city=input ("Enter city")
+college=input("Enter college")
+fav_food =input("Enter fav_food")
+fav_animal=input("Enter fav_animal")
+fav_song =input("enter fav_song")
+
+print(name,dob,city,college,fav_food,fav_animal,fav_song)
