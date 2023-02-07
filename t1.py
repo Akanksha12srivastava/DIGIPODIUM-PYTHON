996aa096c9b2a5d17cc775c7ceffcf243c102ccb
akanksha <akanksha_be21it005@srmcem.ac.in>
2023-02-07 19:46:45 +0530

    conditionl operator

---
+++ t1.py
@@ -0,0 +1,13 @@
+# turtle is a graphic library
+from turtle import *
+
+speed("slowest")
+left(90)              # turn left 90deg
+forward(120)          # then move forward 120 units 
+left(90)
+forward(120)
+left(90)
+forward(120)
+left(90)
+forward(120)
+mainloop()
